99645dcb95fdc73aac2238c9645d3c93971f9e22
bilal <m-bilaljaved612@gmail.com>
2024-08-19 11:51:22 +0500

    website

--- js/main.js
+++ js/main.js
@@ -6,3 +6,67 @@ function toggleNavbar() {
         x.className = "nav-links";
     }
 }
+// for  first slider
+
+var swiper = new Swiper(".mySwiper1", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    initialSlide: 0,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next-custom',
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+
+});
+
+// mySwiper-review
+
+var swiper = new Swiper(".mySwiper-review", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    initialSlide: 0,
+    autoplay: {
+        delay: 2300,
+        disableOnInteraction: false,
+    },
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+
+});
